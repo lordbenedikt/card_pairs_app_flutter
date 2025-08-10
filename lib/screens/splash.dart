5ac7079aaefdce22fb55c2dca6d86da264b79bbb
lordbenedikt <66784958+lordbenedikt@users.noreply.github.com>
2025-08-10 17:58:14 +0200

    change titel

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -7,7 +7,7 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Chat'),
+        title: const Text('Memory App'),
       ),
       body: const Center(
         child: Text('Loading...'),
